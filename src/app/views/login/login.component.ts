113b2782a8c3d32d9b8e38bc56f550f223957fcc
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-03-25 20:27:50 +0200

    sign up

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -9,31 +9,87 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-  form: FormGroup; 
+  
+  loginForm: FormGroup; 
+  signUpForm: FormGroup;
+
+  public registerMode = false;
+
   constructor(
     public fb: FormBuilder,
     private _api: ApiService,
     private _auth: AuthService,
     private router: Router
   ) 
-  { this.form = this.fb.group({ 
+  { 
+    this.loginForm = this.fb.group({ 
       username: ['', Validators.required], 
-      password:['', Validators.required] 
-    })
+      password: ['', Validators.required] 
+    }),
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthday: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      repeat_password: ['', Validators.required],
+      sex: ['M'],
+      tel: [''],
+      email: [''],
+      is_coach: [false],},
+      {validator: this.mainValidator}
+    )
   }
 
   ngOnInit(): void {}
 
+  mainValidator(formGroup: FormGroup): void {
+    const password = formGroup.get('password')?.value;
+    const repeatPassword = formGroup.get('repeat_password')?.value;
+
+    const email = formGroup.get('email')?.value;
+    const tel = formGroup.get('tel')?.value;
+
+    if (password !== repeatPassword) {
+      formGroup.get('repeat_password')?.setErrors({ mismatch: true });
+    } else {
+      formGroup.get('repeat_password')?.setErrors(null);
+    }
+
+    if (email === '' && tel === '') {
+      formGroup.get('email')?.setErrors({ required: true });
+      formGroup.get('tel')?.setErrors({ required: true });
+    }
+  }
+
   login(): void{ 
-    let b = this.form.value;
+    let b = this.loginForm.value;
     this._api.postTypeRequest('api/login', b).subscribe((res: any) => { 
       if(res.access_token){
         this._auth.setDataInLocalStorage('token', res.access_token) 
+        this._auth.userInfo = res.user;
         this.router.navigate(['/']) 
       } 
     }, err => { 
       console.log(err) 
     });
+  }
+
+  signup(): void{
+    let b = this.signUpForm.value;
+    this._api.postTypeRequest('api/signup', b).subscribe((res: any) => {
+      if(res.access_token){
+        this._auth.setDataInLocalStorage('token', res.access_token);
+        this._auth.userInfo = res.user;
+        this.router.navigate(['/'])
+      }
+    }, err => {
+      console.log(err)
+    });
+  }
+
+  registerModeChange(value: boolean): void{
+    this.registerMode = value;
   }
 
 }
