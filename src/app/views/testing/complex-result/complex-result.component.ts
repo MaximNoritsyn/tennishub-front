43e496ab4a07c03cdc7da877f2fe7a0ed9f70a57
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-04-03 20:24:51 +0300

    common results page

--- src/app/views/testing/complex-result/complex-result.component.ts
+++ src/app/views/testing/complex-result/complex-result.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { ApiService } from '../../../services/api.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-complex-result',
   templateUrl: './complex-result.component.html',
-  styleUrls: ['./complex-result.component.css']
+  styleUrls: ['./complex-result.component.css',
+    '../court.css',
+    '../../testing/mobility/mobility.component.css']
 })
 export class ComplexResultComponent implements OnInit {
 
-  constructor() { }
+  idgruptest: string = '';
+  guid: string = '';
+  testEvent: any = {
+    "person":
+      { "first_name": "", "last_name": "", "birthday": "" },
+    "assessor": "", "date": "", "venue": ""
+  };
+
+  mobilityArea: string = '';
+  itnArea: number = 0;
+
+  constructor(
+    private _api: ApiService,
+    public _auth: AuthService,
+    public _router: Router,
+    private _activeRoute: ActivatedRoute
+  ) {
+
+  }
 
   ngOnInit() {
+    this._activeRoute.params.subscribe((params: Params) => {
+      this.guid = params['idtest'];
+      this.idgruptest = params['idgruptest'];
+      this.updateTestEvent();
+      this.updateMobilityArea();
+    });
+  }
+
+  updateTestEvent() {
+    this._api.getTypeRequestParams('api/testevent/', { 'id_db': this.guid }).subscribe((data: any) => {
+      this.testEvent = data;
+      this.itnArea = this.testEvent['itn']
+    });
+  }
+
+  updateMobilityArea() {
+    var _params = {
+      'guid': this.guid,
+      'stage_number': 0,
+      'task': 'mobility'
+    };
+    this._api.getTypeRequestParams('api/detailsserving', _params).subscribe((data: any) => {
+      this.mobilityArea = data['first_bounce'];
+    });
   }
 
 }
